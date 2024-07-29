57fba226a0656dcb6ae6ce9fbdd1fe0d162a7312
Fredpelleg <fpelleg@gmail.com>
2024-07-29 10:21:20 +0200

    Update create-checkout-session.js
    Lundi 29 Juillet 10:21

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,34 +1,27 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-//const stripe = require('stripe')('sk_test_51HyIanFP8glmTVRLMRyos2ErKwWGCTQRSO50GSNUqWkXwBXEJxnSrKGQsoCRBWW5c76AGC3ZydVytVwsTCVQAMMt004ydOJ0Jv'); // Utilisation directe de la clé secrète
-exports.handler = async (event) => {
-  console.log('Using Stripe Secret Key:', process.env.STRIPE_SECRET_KEY; // Vérifiez la clé ici
+const stripe = require('stripe')('sk_test_51HyIanFP8glmTVRLMRyos2ErKwWGCTQRSO50GSNUqWkXwBXEJxnSrKGQsoCRBWW5c76AGC3ZydVytVwsTCVQAMMt004ydOJ0Jv'); // Utilisez la nouvelle clé secrète
 
+exports.handler = async (event) => {
+  console.log('Using Stripe Secret Key:', 'sk_test_51HyIanFP8glmTVRLMRyos2ErKwWGCTQRSO50GSNUqWkXwBXEJxnSrKGQsoCRBWW5c76AGC3ZydVytVwsTCVQAMMt004ydOJ0Jv'); // Vérifiez la clé ici
 
   const { amount, currency } = JSON.parse(event.body);
 
   try {
-    const session = await stripe.checkout.sessions.create({
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: currency,
       payment_method_types: ['card'],
-      line_items: [{
-        price_data: {
-          currency,
-          product_data: {
-            name: 'Caution de Réservation',
-          },
-          unit_amount: amount,
-        },
-        quantity: 1,
-      }],
-      mode: 'payment',
-      success_url: 'https://musical-mousse-b4388f.netlify.app/success.html',
-      cancel_url: 'https://musical-mousse-b4388f.netlify.app/cancel.html',
+      capture_method: 'manual', // Pour créer une autorisation
     });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ url: session.url }),
+      body: JSON.stringify({ clientSecret: paymentIntent.client_secret, id: paymentIntent.id }),
     };
   } catch (error) {
-    console.log('Error creating session:', error.message); // Log l'erreur pour plus de détails
+    console.log('Error creating PaymentIntent:', error.message);
     return {
-      statusCode:
+      statusCode: 400,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
